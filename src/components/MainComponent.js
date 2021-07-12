5cd9848e45176b7aa8c2c9b177b2fee9e3785f5b
Lakita Gaither <lakitagaither@yahoo.com>
2021-07-12 17:19:43 -0500

    Added Header and Main Component

---
+++ src/components/MainComponent.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Header from './HeaderComponent';
+
+
+
+
+class Main extends Component {
+    render() {
+        //const HomePage = () => {
+            //return (
+                //<Home />
+            //);
+        //};
+        return(
+            <div>
+                <Header />
+            </div>
+        );
+        
+    }
+}
+
+export default Main;
