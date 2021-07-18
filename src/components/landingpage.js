ebc37bb9548ae7cf53f8e28f436d232e16c781a0
Lakita Gaither <lakitagaither@yahoo.com>
2021-07-18 15:01:36 -0500

    Reverted to React-MDL Finished Portfolio

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -3,50 +3,58 @@ import { Grid, Cell } from 'react-mdl';
 import mecircle from '../assets/images/mecircle.png';
 
 class Landing extends Component {
-    render() {
-        return(
-            <div style={{width: '100%', margin: 'auto'}}>
-                <Grid className="landing-grid">
-                    <Cell col={12}>
-                        <img 
-                        src={mecircle}
-                        alt="lakita gaither"
-                        className="avatar-image"
-                        />
-                        <div className="banner-text">
-                            <h1>Full Stack Web Developer</h1>
-                            <hr/>
-                            <p>JavaScript | HTML/CSS | Bootstrap | MongoDB | 
-                               Express | React | React Native | Node 
-                            </p>
-                            <div className="social-links">
+  render() {
+    return (
+      <div style={{ width: '100%', margin: 'auto' }}>
+        <Grid className='landing-grid'>
+          <Cell col={12}>
+            <img src={mecircle} alt='lakita gaither' className='avatar-image' />
+            <div className='banner-text'>
+              <h1>Full-Stack Web Developer</h1>
+              <hr />
+              <p>
+                JavaScript | HTML/CSS | Bootstrap | MongoDB | Express | React |
+                React Native | Node
+              </p>
+              <div className='social-links'>
+                {/* LinkedIn */}
+                <a
+                  href='https://www.linkedin.com/in/lakitagaither/'
+                  rel='noopener noreferrer'
+                  target='_blank'
+                >
+                  <i className='fa fa-linkedin-square' aria-hidden='true' />
+                </a>
 
-                                {/* LinkedIn */}
-                                <a href="https://www.linkedin.com/in/lakitagaither/" rel="noopener noreferrer" target="_blank">
-                                    <i className="fa fa-linkedin-square" aria-hidden="true" />
-                                </a>
+                {/* Github */}
+                <a
+                  href='https://github.com/KitaGaither'
+                  rel='noopener noreferrer'
+                  target='_blank'
+                >
+                  <i className='fa fa-github-square' aria-hidden='true' />
+                </a>
 
-                                {/* Github */}
-                                <a href="https://github.com/KitaGaither" rel="noopener noreferrer" target="_blank">
-                                    <i className="fa fa-github-square" aria-hidden="true" />
-                                </a>
+                {/* Freecodecamp */}
+                <a
+                  href='https://www.freecodecamp.org/kitagaither'
+                  rel='noopener noreferrer'
+                  target='_blank'
+                >
+                  <i className='fa fa-free-code-camp' aria-hidden='true' />
+                </a>
 
-                                {/* Freecodecamp */}
-                                <a href="https://www.freecodecamp.org/kitagaither" rel="noopener noreferrer" target="_blank">
-                                    <i className="fa fa-free-code-camp" aria-hidden="true" />
-                                </a>
-
-                                {/* Youtube
+                {/* Youtube
                                 <a href="http://google.com" rel="noopener noreferrer" target="_blank">
                                     <i className="fa fa-youtube-square" aria-hidden="true" />
                                 </a>
                                 */}
-                            </div>
-                        </div>
-                    </Cell>
-                </Grid>
+              </div>
             </div>
-        );
-    }
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
 export default Landing;
